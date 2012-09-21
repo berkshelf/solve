1151ae48b33ce891e6419417f9bb1e9a36829169
Jamie Winsor <jamie@vialstudios.com>
2012-09-21 12:30:56 -0700

    slightly refactor Version class

--- lib/solve/version.rb
+++ lib/solve/version.rb
@@ -1,16 +1,38 @@
 module Solve
+  # @author Jamie Winsor <jamie@vialstudios.com>
   class Version
+    class << self
+      # @param [#to_s] version_string
+      #
+      # @raise [InvalidVersionFormat]
+      #
+      # @return [Array]
+      def from_string(version_string)
+        major, minor, patch = case version_string.to_s
+        when /^(\d+)\.(\d+)\.(\d+)$/
+          [ $1.to_i, $2.to_i, $3.to_i ]
+        when /^(\d+)\.(\d+)$/
+          [ $1.to_i, $2.to_i, 0 ]
+        else
+          raise InvalidVersionFormat.new(version_string)
+        end
+      end
+    end
+
     include Comparable
 
     attr_reader :major
     attr_reader :minor
     attr_reader :patch
 
-    # @param [#to_s] ver_str
-    def initialize(ver_str = String.new)
-      @major, @minor, @patch = parse(ver_str)
+    # @param [#to_s] version_string
+    def initialize(version_string = String.new)
+      @major, @minor, @patch = self.class.from_string(version_string.to_s)
     end
 
+    # @param [Solve::Version] other
+    #
+    # @return [Integer]
     def <=>(other)
       [:major, :minor, :patch].each do |method|
         ans = (self.send(method) <=> other.send(method))
@@ -19,6 +41,9 @@ module Solve
       0
     end
 
+    # @param [Solve::Version] other
+    #
+    # @return [Boolean]
     def eql?(other)
       other.is_a?(Version) && self == other
     end
@@ -30,18 +55,5 @@ module Solve
     def to_s
       "#{major}.#{minor}.#{patch}"
     end
-
-    private
-
-      def parse(ver_str = String.new)
-        @major, @minor, @patch = case ver_str.to_s
-        when /^(\d+)\.(\d+)\.(\d+)$/
-          [ $1.to_i, $2.to_i, $3.to_i ]
-        when /^(\d+)\.(\d+)$/
-          [ $1.to_i, $2.to_i, 0 ]
-        else
-          raise InvalidVersionFormat.new(ver_str)
-        end
-      end
   end
 end
--- spec/unit/solve/version_spec.rb
+++ spec/unit/solve/version_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 
 describe Solve::Version do
+  describe "ClassMethods" do
+    subject { Solve::Version }
+
+    describe "::from_sring" do
+      it "returns an array containing 3 elements" do
+        subject.from_string("1.2.0").should have(3).items
+      end
+
+      context "given a string only containing a major and minor version" do
+        it "returns an array containing 3 elements" do
+          subject.from_string("1.2").should have(3).items
+        end
+
+        it "returns a Zero as the third element" do
+          subject.from_string("1.2")[2].should eql(0)
+        end
+      end
+
+      context "given a string with only a major version" do
+        it "raises an InvalidVersionFormat error" do
+          lambda {
+            subject.from_string("1")
+          }.should raise_error(Solve::InvalidVersionFormat)
+        end
+      end
+    end
+  end
+
   subject { Solve::Version.new("1.0.0") }
 
   describe "#to_s" do
