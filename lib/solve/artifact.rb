1ccd2969f082b7ca30c8166070b8cb03c8243485
Jamie Winsor <jamie@vialstudios.com>
2012-09-24 13:36:26 -0700

    simplify interface for artifact

--- lib/solve/artifact.rb
+++ lib/solve/artifact.rb
@@ -58,37 +58,6 @@ module Solve
     end
     alias_method :depends, :dependencies
 
-    # Add a Solve::Dependency to the collection of dependencies 
-    # and return the added Solve::Dependency. No change will be
-    # made if the dependency is already a member of the collection.
-    #
-    # @param [Solve::Dependency] dependency
-    #
-    # @return [Solve::Dependency]
-    def add_dependency(dependency)
-      unless has_dependency?(dependency)
-        @dependencies[Graph.key_for(dependency)] = dependency
-      end
-
-      dependency
-    end
-
-    # @param [Solve::Dependency] dependency
-    #
-    # @return [Solve::Dependency, nil]
-    def remove_dependency(dependency)
-      if has_dependency?(dependency)
-        @dependencies.delete(Graph.key_for(dependency))
-      end
-    end
-
-    # @param [Solve::Dependency] dependency
-    #
-    # @return [Boolean]
-    def has_dependency?(dependency)
-      @dependencies.has_key?(Graph.key_for(dependency))
-    end
-
     # Remove this artifact from the graph it belongs to
     #
     # @return [Solve::Artifact, nil]
@@ -105,6 +74,52 @@ module Solve
     end
 
     private
+
+      # Add a Solve::Dependency to the collection of dependencies 
+      # and return the added Solve::Dependency. No change will be
+      # made if the dependency is already a member of the collection.
+      #
+      # @param [Solve::Dependency] dependency
+      #
+      # @return [Solve::Dependency]
+      def add_dependency(dependency)
+        unless has_dependency?(dependency.name, dependency.constraint)
+          @dependencies[Graph.key_for(dependency)] = dependency
+        end
+
+        get_dependency(dependency.name, dependency.constraint)
+      end
+
+      # Retrieve the dependency from the artifact with the matching name and constraint
+      #
+      # @param [#to_s] name
+      # @param [#to_s] constraint
+      #
+      # @return [Solve::Artifact, nil]
+      def get_dependency(name, constraint)
+        @dependencies.fetch(Graph.dependency_key(name, constraint), nil)
+      end
+
+      # Remove the matching dependency from the artifact
+      #
+      # @param [Solve::Dependency] dependency
+      #
+      # @return [Solve::Dependency, nil]
+      def remove_dependency(dependency)
+        if has_dependency?(dependency)
+          @dependencies.delete(Graph.key_for(dependency))
+        end
+      end
+
+      # Check if the artifact has a dependency with the matching name and constraint
+      #
+      # @param [#to_s] name
+      # @param [#to_s] constraint
+      #
+      # @return [Boolean]
+      def has_dependency?(name, constraint)
+        @dependencies.has_key?(Graph.dependency_key(name, constraint))
+      end
 
       # @return [Array<Solve::Dependency>]
       def dependency_collection
