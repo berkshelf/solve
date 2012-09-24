f1dc6cf0a486b2ec07ed87e0fea2a62842baac8f
Andrew Garson <andrew.garson@gmail.com>
2012-09-24 12:37:43 -0700

    Add back the test for finding the correct version given multiple choices

--- spec/acceptance/solutions_spec.rb
+++ spec/acceptance/solutions_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe "Solutions" do
+  
+  it "chooses the correct artifact for the demands" do
+    graph = Solve::Graph.new
+    graph.artifacts("mysql", "2.0.0")
+    graph.artifacts("mysql", "1.2.0")
+    graph.artifacts("nginx", "1.0.0").depends("mysql", "= 1.2.0")
+
+    result = Solve.it!(graph, [['nginx', '= 1.0.0'], ['mysql']])
+
+    result.should eql("nginx" => "1.0.0", "mysql" => "1.2.0")
+  end
+
   it "chooses the best artifact for the demands" do
     graph = Solve::Graph.new
     graph.artifacts("mysql", "2.0.0")
