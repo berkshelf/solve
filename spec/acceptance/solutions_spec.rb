2d1b7f18ad2e4d5548eaec312b1749364523f1e0
Andrew Garson <andrew.garson@gmail.com>
2012-09-24 16:17:00 -0700

    Add complex test case involving multiple backtracks of root level variables

--- spec/acceptance/solutions_spec.rb
+++ spec/acceptance/solutions_spec.rb
@@ -32,4 +32,38 @@ describe "Solutions" do
       Solve.it!(graph, ['mysql', '>= 2.0.0'])
     }.should raise_error(Solve::Errors::NoSolutionError)
   end
+
+  it "find the correct solution when backtracking in variables introduced via demands" do
+    graph = Solve::Graph.new
+
+    graph.artifacts("D", "1.2.0")
+    graph.artifacts("D", "1.3.0")
+    graph.artifacts("D", "1.4.0")
+    graph.artifacts("D", "2.0.0")
+    graph.artifacts("D", "2.1.0")
+
+    graph.artifacts("C", "2.0.0").depends("D", "= 1.2.0")
+    graph.artifacts("C", "2.1.0").depends("D", ">= 2.1.0")
+    graph.artifacts("C", "2.2.0").depends("D", "> 2.0.0")
+
+    graph.artifacts("B", "1.0.0").depends("D", "= 1.0.0")
+    graph.artifacts("B", "1.1.0").depends("D", "= 1.0.0")
+    graph.artifacts("B", "2.0.0").depends("D", ">= 1.3.0")
+    graph.artifacts("B", "2.1.0").depends("D", ">= 2.0.0")
+
+    graph.artifacts("A", "1.0.0").depends("B", "> 1.0.0")
+    graph.artifacts("A", "1.0.0").depends("C", "= 2.0.0")
+    graph.artifacts("A", "1.0.1").depends("B", "> 1.0.0")
+    graph.artifacts("A", "1.0.1").depends("C", "= 2.1.0")
+    graph.artifacts("A", "1.0.2").depends("B", "> 2.0.0")
+    graph.artifacts("A", "1.0.2").depends("C", "= 2.1.0")
+
+    result = Solve.it!(graph, [['A', '~> 1.0.0'], ['D', ">= 2.0.0"]])
+
+
+    result.should eql("A" => "1.0.2",
+                      "B" => "2.1.0",
+                      "C" => "2.1.0",
+                      "D" => "2.1.0")
+  end
 end
