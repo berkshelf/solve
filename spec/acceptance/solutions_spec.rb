ac3cafb71bb2f56f37418223fbb679d42e445123
Andrew Garson <andrew.garson@gmail.com>
2012-09-25 15:50:01 -0700

    Force complex test case to backtrack through demand introduced variables now that we sort possible values from largest to smallest this no longer happened

--- spec/acceptance/solutions_spec.rb
+++ spec/acceptance/solutions_spec.rb
@@ -33,7 +33,7 @@ describe "Solutions" do
     }.should raise_error(Solve::Errors::NoSolutionError)
   end
 
-  it "find the correct solution when backtracking in variables introduced via demands" do
+  it "find the correct solution when backtracking in variables introduced via demands", focus: true  do
     graph = Solve::Graph.new
 
     graph.artifacts("D", "1.2.0")
@@ -51,18 +51,18 @@ describe "Solutions" do
     graph.artifacts("B", "2.0.0").depends("D", ">= 1.3.0")
     graph.artifacts("B", "2.1.0").depends("D", ">= 2.0.0")
 
-    graph.artifacts("A", "1.0.0").depends("B", "> 1.0.0")
-    graph.artifacts("A", "1.0.0").depends("C", "= 2.0.0")
+    graph.artifacts("A", "1.0.0").depends("B", "> 2.0.0")
+    graph.artifacts("A", "1.0.0").depends("C", "= 2.1.0")
     graph.artifacts("A", "1.0.1").depends("B", "> 1.0.0")
     graph.artifacts("A", "1.0.1").depends("C", "= 2.1.0")
-    graph.artifacts("A", "1.0.2").depends("B", "> 2.0.0")
-    graph.artifacts("A", "1.0.2").depends("C", "= 2.1.0")
+    graph.artifacts("A", "1.0.2").depends("B", "> 1.0.0")
+    graph.artifacts("A", "1.0.2").depends("C", "= 2.0.0")
 
     result = Solve.it!(graph, [['A', '~> 1.0.0'], ['D', ">= 2.0.0"]])
 
 
-    result.should eql("A" => "1.0.2",
-                      "B" => "2.1.0",
+    result.should eql("A" => "1.0.1",
+                      "B" => "2.0.0",
                       "C" => "2.1.0",
                       "D" => "2.1.0")
   end
@@ -95,7 +95,7 @@ describe "Solutions" do
                       "C" => "1.0.0")
   end
 
-  it "finds the correct solution when there is a diamond shaped dependency" do
+  it "finds the correct solution when there is a diamond shaped dependency"do
     graph = Solve::Graph.new
 
     graph.artifacts("A", "1.0.0")
