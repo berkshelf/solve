b17dbea94ab0b9981d256711acd08817ea709064
Jamie Winsor <jamie@vialstudios.com>
2012-09-24 19:57:06 -0700

    add bare function documentation to variable_row / table

--- lib/solve/solver/variable_row.rb
+++ lib/solve/solver/variable_row.rb
@@ -7,12 +7,15 @@ module Solve
       attr_reader :value
       attr_reader :sources
 
+      # @param [String] artifact
+      # @param [String, Symbol] source
       def initialize(artifact, source)
         @artifact = artifact
         @value = nil
         @sources = Array(source)
       end
 
+      # @param [String, Symbol] source
       def add_source(source)
         @sources << source
       end
@@ -33,6 +36,7 @@ module Solve
         !@value.nil?
       end
 
+      # @param [String, Symbol] source
       def remove_source(source)
         @sources.delete(source)
       end
