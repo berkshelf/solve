1af8099bc0a803216da3cbcb3c2214ecbe0291c9
Seth Vargo <sethvargo@gmail.com>
2014-03-23 21:41:59 -0400

    Update the API for Artifact
    This commit updates the API for to be more "Rubyesque". It also includes
    some minor performance enhancements and caching.

--- README.md
+++ README.md
@@ -12,37 +12,46 @@ A Ruby versioning constraint solver implementing [Semantic Versioning 2.0.0](htt
 
 Create a new graph
 
-    graph = Graph.new
+```ruby
+graph = Graph.new
+```
 
 Add an artifact to the graph
 
-    graph.artifacts("nginx", "1.0.0")
+```ruby
+graph.artifact("nginx", "1.0.0")
+```
 
 Now add another artifact that has a dependency
 
-    graph.artifacts("mysql", "1.2.4-alpha.1").depends("openssl", "~> 1.0.0")
+```ruby
+graph.artifact("mysql", "1.2.4-alpha.1").depends("openssl", "~> 1.0.0")
+```
 
 Dependencies can be chained, too
 
-    graph.artifacts("ntp", "1.0.0").depends("build-essential").depends("yum")
+```ruby
+graph.artifact("ntp", "1.0.0")
+  .depends("build-essential")
+  .depends("yum", ">= 1.0.0")
+```
 
 And now solve the graph with some demands
 
-    Solve.it!(graph, ['nginx', '>= 0.100.0'])
+```ruby
+Solve.it!(graph, ['nginx', '>= 0.100.0'])
+```
 
 Or, if you want a topologically sorted solution
-NOTE: This will raise Solve::Errors::UnsortableSolutionError if the solution contains a cycle (which can happen with ruby packages)
+NOTE: This will raise Solve::Errors::UnsortableSolutionError if the solution contains a cycle (which can happen with Ruby packages)
 
-    Solve.it!(graph, ['nginx', '>= 0.100.0'], sorted: true)
-
-### Removing an artifact, or dependency from the graph
-
-    graph.artifacts("nginx", "1.0.0").delete
-
-    artifact.dependencies("nginx", "~> 1.0.0").delete
+```ruby
+Solve.it!(graph, ['nginx', '>= 0.100.0'], sorted: true)
+```
 
 ## Authors
 
+* [Seth Vargo](https://github.com/sethvargo) (<sethvargo@gmail.com>)
 * [Jamie Winsor](https://github.com/reset) (<jamie@vialstudios.com>)
 * [Andrew Garson](andrewGarson) (<agarson@riotgames.com>)
 * [Thibaud Guillaume-Gentil](https://github.com/thibaudgg) ([@thibaudgg](http://twitter.com/thibaudgg))
--- lib/solve/artifact.rb
+++ lib/solve/artifact.rb
@@ -21,43 +21,23 @@ module Solve
     # @param [#to_s] name
     # @param [Solve::Version, #to_s] version
     def initialize(graph, name, version)
-      @graph = graph
-      @name = name
-      @version = Version.new(version)
-      @dependencies = Hash.new
+      @graph        = graph
+      @name         = name
+      @version      = Version.new(version)
+      @dependencies = {}
     end
 
-    # Return the Solve::Dependency from the collection of
-    # dependencies with the given name and constraint.
+    # Check if the artifact has a dependency with the matching name and
+    # constraint
     #
     # @param [#to_s] name
-    # @param [Solve::Constraint, #to_s] constraint
+    # @param [#to_s] constraint
     #
-    # @example adding dependencies
-    #   artifact.depends("nginx") => <#Dependency: @name="nginx", @constraint=">= 0.0.0">
-    #   artifact.depends("ntp", "= 1.0.0") => <#Dependency: @name="ntp", @constraint="= 1.0.0">
-    #
-    # @example chaining dependencies
-    #   artifact.depends("nginx").depends("ntp")
-    #
-    # @return [Solve::Artifact]
-    def depends(name, constraint = ">= 0.0.0")
-      if name.nil?
-        raise ArgumentError, "A name must be specified. You gave: #{args}."
-      end
-
-      dependency = Dependency.new(self, name, constraint)
-      add_dependency(dependency)
-
-      self
+    # @return [Boolean]
+    def dependency?(name, constraint)
+      !dependency(name, constraint).nil?
     end
-
-    # Return the collection of dependencies on this instance of artifact
-    #
-    # @return [Array<Solve::Dependency>]
-    def dependencies
-      @dependencies.collect { |name, dependency| dependency }
-    end
+    alias_method :has_dependency?, :dependency?
 
     # Retrieve the dependency from the artifact with the matching name and constraint
     #
@@ -65,19 +45,41 @@ module Solve
     # @param [#to_s] constraint
     #
     # @return [Solve::Artifact, nil]
-    def get_dependency(name, constraint)
-      @dependencies.fetch(Graph.dependency_key(name, constraint), nil)
+    def dependency(name, constraint)
+      @dependencies["#{name}-#{constraint}"]
     end
 
-    # Remove this artifact from the graph it belongs to
+    # Return the collection of dependencies on this instance of artifact
     #
-    # @return [Solve::Artifact, nil]
-    def delete
-      unless graph.nil?
-        result = graph.remove_artifact(self)
-        @graph = nil
-        result
+    # @return [Array<Solve::Dependency>]
+    def dependencies
+      @dependencies.values
+    end
+
+    # Return the Solve::Dependency from the collection of
+    # dependencies with the given name and constraint.
+    #
+    # @param [#to_s] name
+    # @param [String] constraint
+    #
+    # @example Adding dependencies
+    #   artifact.depends('nginx')
+    #     #=> #<Dependency nginx (>= 0.0.0)>
+    #   artifact.depends('ntp', '= 1.0.0')
+    #     #=> #<Dependency ntp (= 1.0.0)>
+    #
+    # @example Chaining dependencies
+    #   artifact
+    #     .depends('nginx')
+    #     .depends('ntp', '~> 1.3')
+    #
+    # @return [Solve::Artifact]
+    def depends(name, constraint = '>= 0.0.0')
+      unless dependency?(name, constraint)
+        @dependencies["#{name}-#{constraint}"] = Dependency.new(self, name, constraint)
       end
+
+      self
     end
 
     def to_s
@@ -89,8 +91,8 @@ module Solve
     # @return [Boolean]
     def ==(other)
       other.is_a?(self.class) &&
-        self.name == other.name &&
-        self.version == other.version
+      self.name == other.name &&
+      self.version == other.version
     end
     alias_method :eql?, :==
 
@@ -100,32 +102,5 @@ module Solve
     def <=>(other)
       self.version <=> other.version
     end
-
-    private
-
-      # Add a Solve::Dependency to the collection of dependencies 
-      # and return the added Solve::Dependency. No change will be
-      # made if the dependency is already a member of the collection.
-      #
-      # @param [Solve::Dependency] dependency
-      #
-      # @return [Solve::Dependency]
-      def add_dependency(dependency)
-        unless has_dependency?(dependency.name, dependency.constraint)
-          @dependencies[Graph.key_for(dependency)] = dependency
-        end
-
-        get_dependency(dependency.name, dependency.constraint)
-      end
-
-      # Check if the artifact has a dependency with the matching name and constraint
-      #
-      # @param [#to_s] name
-      # @param [#to_s] constraint
-      #
-      # @return [Boolean]
-      def has_dependency?(name, constraint)
-        @dependencies.has_key?(Graph.dependency_key(name, constraint))
-      end
   end
 end
