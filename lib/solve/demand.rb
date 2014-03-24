7d930723ae4621c8fe35eb6c5e4f335f2dbbbeed
Seth Vargo <sethvargo@gmail.com>
2014-03-23 21:42:14 -0400

    Use Constraint.coerce in Demand

--- lib/solve/demand.rb
+++ lib/solve/demand.rb
@@ -18,14 +18,10 @@ module Solve
     # @param [Solve::Solver] solver
     # @param [#to_s] name
     # @param [Solve::Constraint, #to_s] constraint
-    def initialize(solver, name, constraint = ">= 0.0.0")
-      @solver = solver
-      @name   = name
-      @constraint = if constraint.is_a?(Solve::Constraint)
-        constraint
-      else
-        Constraint.new(constraint.to_s)
-      end
+    def initialize(solver, name, constraint = DEFAULT_CONSTRAINT)
+      @solver     = solver
+      @name       = name
+      @constraint = Constraint.coerce(constraint)
     end
 
     def to_s
