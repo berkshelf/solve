69eb3192a84288d485bcd977ac52692fd656af5f
Seth Vargo <sethvargo@gmail.com>
2014-03-23 21:44:56 -0400

    Constraint style update

--- lib/solve/constraint.rb
+++ lib/solve/constraint.rb
@@ -186,15 +186,15 @@ module Solve
     # Returns true or false if the given version would be satisfied by
     # the version constraint.
     #
-    # @param [#to_s] target_version
+    # @param [Version, #to_s] target
     #
     # @return [Boolean]
-    def satisfies?(target_version)
-      target_version = Version.new(target_version.to_s)
+    def satisfies?(target)
+      target = Version.coerce(target.to_s)
 
-      return false if !version.zero? && greedy_match?(target_version)
+      return false if !version.zero? && greedy_match?(target)
 
-      compare(target_version)
+      compare(target)
     end
 
     # @param [Object] other
