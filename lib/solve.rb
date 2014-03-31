49c975ea6d2ae03998256f4829fad7626d0ad900
Seth Vargo <sethvargo@gmail.com>
2014-03-31 00:30:21 -0400

    Require the new universe class

--- lib/solve.rb
+++ lib/solve.rb
@@ -7,6 +7,7 @@ module Solve
   require_relative 'solve/errors'
   require_relative 'solve/graph'
   require_relative 'solve/solver'
+  require_relative 'solve/universe'
   require_relative 'solve/version'
   require_relative 'solve/tracers'
 
