f6b827047af70c01d3a0c5a3d0c78074521b448c
Jamie Winsor <jamie@vialstudios.com>
2014-04-04 16:29:11 -0700

    remove tracers
    these are no longer used for output debug info

--- lib/solve.rb
+++ lib/solve.rb
@@ -8,7 +8,6 @@ module Solve
   require_relative 'solve/graph'
   require_relative 'solve/solver'
   require_relative 'solve/version'
-  require_relative 'solve/tracers'
 
   class << self
     # @return [Solve::Formatter]
@@ -24,10 +23,7 @@ module Solve
     # @param [Array<Solve::Demand>, Array<String, String>] demands
     #
     # @option options [#say] :ui (nil)
-    #   a ui object for output, this will be used to output from a Solve::Tracers::HumanReadable if
-    #   no other tracer is provided in options[:tracer]
-    # @option options [AbstractTracer] :tracer (nil)
-    #   a Tracer object that is used to format and output tracing information
+    #   a ui object for output
     # @option options [Boolean] :sorted (false)
     #   should the output be a sorted list rather than a Hash
     #
@@ -35,7 +31,6 @@ module Solve
     #
     # @return [Hash]
     def it!(graph, demands, options = {})
-      @tracer = options[:tracer] || Solve::Tracers.build(options[:ui])
       Solver.new(graph, demands, options[:ui]).resolve(options)
     end
   end
